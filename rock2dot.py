1404e35a74dd449c47a5377c7602b5ce4c34898b
Moritz Schilling <moritz.schilling@dfki.de>
2017-03-07 09:09:35 +0100

    In rock ruby scripts the order of connect_to statements do not imply port type Kudos to Marco Pagnamenta
    Signed-off-by: Moritz Schilling <moritz.schilling@dfki.de>

--- rock2dot.py
+++ rock2dot.py
@@ -4,7 +4,7 @@ import sys
 infile, outfile = sys.argv[1], sys.argv[2]
 inf,outf = open(infile), open(outfile,"w")
 
-outf.write("digraph \"" + infile + "\" {\n")
+outf.write("graph \"" + infile + "\" {\n")
 outf.write("graph [nodesep=1.5 ranksep=1.0];\n")
 outf.write("node [margin=0.2 shape=box];\n")
 
@@ -16,7 +16,7 @@ for line in inf.readlines():
             targetSplit = splitted[1].lstrip().split(".")
             sourceNode,sourcePort = "\""+sourceSplit[0]+"\"","\""+sourceSplit[1]+"\""
             targetNode,targetPort = "\""+targetSplit[0]+"\"","\""+targetSplit[1]+"\""
-            outf.write(sourceNode + " -> " + targetNode + "[ taillabel=" + sourcePort + " headlabel=" + targetPort + " ];\n")
+            outf.write(sourceNode + " -- " + targetNode + "[ taillabel=" + sourcePort + " headlabel=" + targetPort + " ];\n")
 
 outf.write("}\n")
 
